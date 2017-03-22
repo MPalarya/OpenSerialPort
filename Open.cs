715a105a354bdceebfd4f7a0ea66605a67e0721f
Michael Palarya <palarya@gmail.com>
2017-03-22 17:52:26 +0200

    Update Open.cs

--- Open.cs
+++ Open.cs
@@ -8,76 +8,120 @@
                 // everything that we read is appended to a List<byte>
                 // we begin loop-reading until we get first 2 bytes - indicating length of msg
                 // having the length, we know when to stop reading and raise the outer event
-                // during this process we maintain a timeout timer
+                // each incomplete message initiates a timeout-timer
 
-                int readSoFar = 0;
-                int lenToRead = 0;
-                bool abort = false;
+                int lenToRead = -1;
                 List<byte> data = new List<byte>();
 
-                var timeout = new System.Timers.Timer(1000);
-                timeout.Elapsed += delegate(object sender, ElapsedEventArgs e)
+                portInitData(data, out lenToRead);
+
+                // define timer
+                _timeoutTimer = new System.Timers.Timer(1000) { AutoReset = false };
+                _timeoutTimer.Elapsed += delegate(object sender, ElapsedEventArgs e)
                 {
-                    lock (data)
-                    {
-                        abort = true;
-                        readSoFar = 0;
-                        lenToRead = -1; // make sure: lenToRead != readSoFar
-                        data.Clear();
-                    }
+                    portInitData(data, out lenToRead);
                 };
 
                 byte[] buffer = new byte[1024];
                 Action kickoffRead = null;
                 kickoffRead = delegate
                 {
-                    if (port != null && port.IsOpen)
+                    try
                     {
-                        try
-                        {
-                            abort = false;
-                            timeout.Start();
-
-                            port.BaseStream.BeginRead(buffer, 0, buffer.Length,
-                                delegate(IAsyncResult ar)
+                        port.BaseStream.BeginRead(buffer, 0, buffer.Length,
+                            delegate(IAsyncResult ar)
+                            {
+                                try
                                 {
-                                    try
+                                    int currentReadLength = port.BaseStream.EndRead(ar);
+                                    byte[] receivedBytes = new byte[currentReadLength];
+                                    Buffer.BlockCopy(buffer, 0, receivedBytes, 0, currentReadLength);
+                                    lock (data)
                                     {
-                                        int actualReadLength = port.BaseStream.EndRead(ar);
-                                        byte[] receivedBytes = new byte[actualReadLength];
-                                        Buffer.BlockCopy(buffer, 0, receivedBytes, 0, actualReadLength);
+                                        data.AddRange(receivedBytes);
 
-                                        if (!abort)
+                                        // we don't have enough bytes to deduce lenToRead
+                                        if (data.Count < 2)
                                         {
-                                            lock (data)
-                                            {
-                                                data.AddRange(receivedBytes);
-                                                readSoFar += actualReadLength;
-                                                if (data.Count >= 2)
-                                                {
-                                                    byte[] lenArray = new byte[] { data[0], data[1] };
-                                                    lenToRead = EndianBitConverter.Little.ToInt16(lenArray, 0); // TODO: is it little endian? <<< <<< <<<
-                                                }
-                                            }
+                                            // error - not possible, lenToRead is deduced by data
+                                            if (lenToRead >= 2)
+                                                portInitData(data, out lenToRead);
+
+                                            // have read some bytes, but not enough
+                                            else
+                                                // not finished yet - not enough bytes to deduce lenToRead
+                                                _timeoutTimer.Stop();
+                                                _timeoutTimer.Start();
                                         }
 
-                                        // raise event
-                                        if (readSoFar == lenToRead && this.BytesReceived != null)
-                                            BytesReceived(this, data.ToArray());
+                                        // we read enough bytes to know lenToRead
+                                        else
+                                        {
+                                            if (lenToRead < 2)
+                                            {
+                                                // get lenToRead
+                                                byte[] lenArray = new byte[] { data[0], data[1] };
+                                                lenToRead = EndianBitConverter.Little.ToInt16(lenArray, 0);
+
+                                                // error - length must be > 2
+                                                if (lenToRead < 2)
+                                                    portInitData(data, out lenToRead);
+                                            }
+
+                                            // either it was already > 2, or we just calculated it in previous 'if'
+                                            // careful not to use 'else' here:
+                                            if (lenToRead >= 2)
+                                            {
+                                                if (lenToRead == data.Count && this.BytesReceived != null)
+                                                {
+                                                    // avoid loss of data
+                                                    _timeoutTimer.Stop();
+
+                                                    byte[] tmpArray = data.ToArray();
+
+                                                    portInitData(data, out lenToRead);
+
+                                                    // finished - raise event
+                                                    BytesReceived(this, tmpArray);
+                                                }
+
+                                                // not finished yet - still bytes to read until lenToRead
+                                                else if (lenToRead > data.Count)
+                                                {
+                                                    _timeoutTimer.Stop();
+                                                    _timeoutTimer.Start();
+                                                }
+
+                                                // error - read more than we should, msg is invalid
+                                                else
+                                                    portInitData(data, out lenToRead);
+                                            }
+                                        }
                                     }
-                                    catch (Exception ex)
-                                    {
-                                        // handle app serail error
-                                    }
-                                    kickoffRead();
-                                }, null);
-                        }
-                        catch (Exception)
+                                }
+                                catch (Exception ex)
+                                {
+
+                                }
+                                kickoffRead();
+                            }, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        // try to "reconnect" again after 1 sec
+                        Thread.Sleep(1000);
+                        if ((port != null) && (!port.IsOpen))
                         {
-                            // try to "reconnect" again after 1 sec
-                            Thread.Sleep(1000);
-                            kickoffRead();
+                            try
+                            {
+                                port.Open();
+                            }
+                            catch (Exception exx)
+                            {
+
+                            }
                         }
+                        kickoffRead();
                     }
                 };
                 kickoffRead();
